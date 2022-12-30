4891f7725f197ab7b6c893d6b2f74a781d3d6d0f
williamszk <wllmszk@gmail.com>
2022-12-29 17:23:12 -0800

    start again django schwarzmuller, review section

---
+++ udemy-schwarzmuller/221229_01/notes.sh
@@ -0,0 +1,25 @@
+
+python -m venv .venv
+
+pip install Django
+
+django-admin startproject django_course_site
+
+cd django_course_site
+python manage.py startapp meetups
+
+python manage.py runserver
+
+
+
+
+
+
+
+
+
+
+
+
+
+
