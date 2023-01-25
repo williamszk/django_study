f4f39a522daab3775761e3170aa417c929873076
williamszk <wllmszk@gmail.com>
2023-01-25 09:17:25 -0300

    django study

--- udemy-schwarzmuller/230109_01/django_course_site/meetups/urls.py
+++ udemy-schwarzmuller/230109_01/django_course_site/meetups/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("<slug:meetup_slug>/", views.meetup_details, name="meetup_details"),
+    path("success", views.confirm_registration, name="confirm-registration"),
+    path("<slug:meetup_slug>/", views.meetup_details, name="meetup-details"),
 ]
--- udemy-schwarzmuller/230109_01/django_course_site/meetups/views.py
+++ udemy-schwarzmuller/230109_01/django_course_site/meetups/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Meetup
+from .forms import RegistrationForm
 
 
 def index(request):
@@ -14,8 +15,28 @@ def index(request):
 def meetup_details(request, meetup_slug):
     try:
         selected_meetup = Meetup.objects.get(slug=meetup_slug)
-        context = {"meetup_found": True, "meetup": selected_meetup}
+        if request.method == "GET":
+            registration_form = RegistrationForm()
+
+        elif request.method == "POST":
+            registration_form = RegistrationForm(request.POST)
+            if registration_form.is_valid():
+                participant = registration_form.save()
+                selected_meetup.participant.add(participant)
+                return redirect("confirm-registration")
+
+        context = {
+            "meetup_found": True,
+            "meetup": selected_meetup,
+            "form": registration_form,
+        }
         return render(request, "meetups/meetup-details.html", context)
+
     except Exception:
         context = {"meetup_found": False}
         return render(request, "meetups/meetup-details.html", context)
+
+
+def confirm_registration(request):
+    context = {}
+    return render(request, "meetups/registration-success.html", context)
